84feccd984511b2404a98d33fc0d913c0c88dff3
Marco Zulian <codebymarco1@gmail.com>
2025-04-29 16:01:04 -0300

    implements file search route

--- app/main.go
+++ app/main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
 )
 
+var directory = flag.String("directory", "", "Specify the directory from where to search for files")
+
 func main() {
 	fmt.Println("Logs from your program will appear here!")
+	flag.Parse()
 
 	server := NewServer(4221)
 	server.AddHandler("^/$", EmptyPathHandler)
 	server.AddHandler("^/echo", EchoPathHandler)
-  server.AddHandler("^/user-agent", UserAgentHandler)
+	server.AddHandler("^/user-agent", UserAgentHandler)
+	server.AddHandler("^/files", FilesPathHandler)
 
 	server.Serve()
 }
@@ -25,11 +31,11 @@ func EchoPathHandler(request *Request) *Response {
 	response := NewResponse()
 
 	echoPathParts := strings.SplitN(request.Path, "/", 3)
-  
-  var echoStr string
-  if len(echoPathParts) > 2 {
-    echoStr = echoPathParts[2]
-  }
+
+	var echoStr string
+	if len(echoPathParts) > 2 {
+		echoStr = echoPathParts[2]
+	}
 
 	response.AddHeader("Content-Type", "text/plain")
 	response.AddHeader("Content-Length", strconv.Itoa(len(echoStr)))
@@ -39,13 +45,37 @@ func EchoPathHandler(request *Request) *Response {
 }
 
 func UserAgentHandler(request *Request) *Response {
-  response := NewResponse()
+	response := NewResponse()
 
-  userAgent := request.GetHeader("User-Agent")
-  
-  response.AddHeader("Content-Type", "text/plain")
-  response.AddHeader("Content-Length", strconv.Itoa(len(userAgent)))
-  response.SetBody(userAgent)
+	userAgent := request.GetHeader("User-Agent")
 
-  return response
+	response.AddHeader("Content-Type", "text/plain")
+	response.AddHeader("Content-Length", strconv.Itoa(len(userAgent)))
+	response.SetBody(userAgent)
+
+	return response
+}
+
+func FilesPathHandler(request *Request) *Response {
+	response := NewResponse()
+
+	filePathParts := strings.SplitN(request.Path, "/", 3)
+
+	if len(filePathParts) != 3 {
+		response.SetStatus(404)
+		return response
+	}
+
+	data, err := os.ReadFile(fmt.Sprintf("%s%s", *directory, filePathParts[2]))
+	if err != nil {
+		fmt.Println(err)
+		response.SetStatus(404)
+		return response
+	}
+
+	response.SetBody(string(data))
+	response.AddHeader("Content-Type", "application/octet-stream")
+	response.AddHeader("Content-Length", strconv.Itoa(len(data)))
+
+	return response
 }
